29b04e282cf560225564ff4f3cb0a05bf1c9c603
reshad.khoyratty <reshad.khoyratty@berger-levrault.com>
2026-04-17 17:47:05 +0200

    set base href for resource loading and update script source path

--- tco-roi-advisor-main/vite.config.ts
+++ tco-roi-advisor-main/vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/bl-elements/tco-roi-advisor-main/",
   server: {
     host: "::",
     port: 8080,
